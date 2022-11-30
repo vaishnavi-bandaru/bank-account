2a2637edb99b4b680cdc7c20f9aa0961ebc29cfd
Vaishnavi <vaishnavi.bandaru@thoughtworks.com>
2022-11-30 22:54:27 +0530

    [Vaishnavi] Refactor. customer principal service test

--- src/test/java/com/example/bankaccount/service/CustomerPrincipalServiceTest.java
+++ src/test/java/com/example/bankaccount/service/CustomerPrincipalServiceTest.java
@@ -4,38 +4,32 @@ import com.example.bankaccount.controller.request.CustomerSignupRequest;
 import com.example.bankaccount.exceptions.AccountAlreadyExistsException;
 import com.example.bankaccount.model.Customer;
 import com.example.bankaccount.repo.CustomerRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class CustomerPrincipalServiceTest {
-    @Autowired
+    @Mock
     CustomerRepository customerRepository;
-
-    @BeforeEach
-    void before() {
-        customerRepository = mock(CustomerRepository.class);
-    }
-
-    @AfterEach
-    public void after() {
-        customerRepository.deleteAll();
-    }
+    @InjectMocks
+    CustomerPrincipalService customerPrincipalService;
 
     @Test
     void shouldBeAbleToSaveCustomer() throws AccountAlreadyExistsException {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         CustomerSignupRequest customerSignupRequest = new CustomerSignupRequest("abc", "abc@gmail.com", "password");
         Customer customer = new Customer("abc", "abc@gmail.com", bCryptPasswordEncoder.encode("password"));
-        CustomerPrincipalService customerPrincipalService = new CustomerPrincipalService(customerRepository);
 
         customerPrincipalService.save(customerSignupRequest, customer);
 
@@ -43,13 +37,12 @@ public class CustomerPrincipalServiceTest {
     }
 
     @Test
-    void shouldThrowAccountAlreadyExistsExceptionWhenEmailAlreadyLinkedToAnAccount() { BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    void shouldThrowAccountAlreadyExistsExceptionWhenEmailAlreadyLinkedToAnAccount() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         CustomerSignupRequest customerSignupRequest = new CustomerSignupRequest("abc", "abc@gmail.com", "password");
         Customer customer = new Customer("abc", "abc@gmail.com", bCryptPasswordEncoder.encode("password"));
         when(customerRepository.findByEmail("abc@gmail.com")).thenReturn(Optional.of(customer));
 
-        CustomerPrincipalService customerPrincipalService = new CustomerPrincipalService(customerRepository);
-
-        assertThrows(AccountAlreadyExistsException.class, ()-> customerPrincipalService.save(customerSignupRequest,customer));
+        assertThrows(AccountAlreadyExistsException.class, () -> customerPrincipalService.save(customerSignupRequest, customer));
     }
 }
